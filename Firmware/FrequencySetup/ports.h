2eb6999f3911348a02bfac5f2fce70ae1cd5afc3
imoralesgt <ivan.rene.morales@gmail.com>
2015-09-25 11:51:53 -0600

    PWM without interrupts

--- Firmware/FrequencySetup/ports.h
+++ Firmware/FrequencySetup/ports.h
@@ -5,9 +5,9 @@
 
 //PORT 1
 #define LED_OK    (0x0001); //P1.0
-#define SOUND_OUT (0x0004); //P1.2
-#define FREQ_OUT  (0x0040); //P1.6
+#define FREQ_OUT  (0x0004); //P1.2
 #define PUSH_BTN  (0x0008); //P1.3
+#define SOUND_OUT (0x0040); //P1.6
 
 //PORT 2
 #define PULSE_IN  (0x0008); //P2.3
