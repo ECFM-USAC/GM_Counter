8fabeeb8e46016dd31f50b83029380d8c0197182
Iván Morales <ivan.rene.morales@gmail.com>
2016-01-18 09:21:19 -0600

    RESYNC

--- Firmware/FrequencySetup/Debug/main.pp
+++ Firmware/FrequencySetup/Debug/main.pp
@@ -1,10 +1,10 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
-main.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
-main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
-main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
 main.obj: ../ports.h
 main.obj: ../setup.h
 main.obj: ../lcd.h
@@ -15,10 +15,10 @@ main.obj: ../setup.h
 main.obj: ../hvValues.h
 
 ../main.c: 
-c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
-c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
-c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
-c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
 ../ports.h: 
 ../setup.h: 
 ../lcd.h: 
